4120fa963698751437d9dac4ea44fd9ed413cc75
Vvpskvd <127900056+vvpskvd@users.noreply.github.com>
2024-05-12 21:53:14 -0500

    SQL
    datos de usuarios para ingreso de login

---
+++ sql/usuarios_login.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-05-2024 a las 04:50:54
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `log_enfsanar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_login`
+--
+
+CREATE TABLE `usuarios_login` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `contacto` bigint(40) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `rol` varchar(50) DEFAULT NULL,
+  `contra` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios_login`
+--
+
+INSERT INTO `usuarios_login` (`id`, `usuario`, `contacto`, `email`, `rol`, `contra`) VALUES
+(1, 'Luz Gaitan', 3168925532, 'luzanbussines@gmail.com', 'medico', 'lg_12345'),
+(2, 'Daniel Arevalo', 3028530092, 'danielarevalobussines@gmail.com', 'recepcionista', 'da_12345'),
+(3, 'Jefferson Chaustre', 3166846822, 'jeffersonbussines@gmail.com', 'auxiliar', 'jc_12345');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios_login`
+--
+ALTER TABLE `usuarios_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_login`
+--
+ALTER TABLE `usuarios_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
